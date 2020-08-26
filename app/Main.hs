fc29b0d7676aa1cf9db13247e061db66abba0d49
Kevin Power <kevin.power.97.kp@gmail.com>
2020-08-26 12:47:25 +0100

    numbers of lists

--- app/Main.hs
+++ app/Main.hs
@@ -9,12 +9,16 @@ main = do
 
   csvData <- readFile file
 
-  -- cleaning section --
-  let firstPass = cleanRoundOne csvData
-  print $ firstPass
-
   let candidates = getCandidates csvData
   putStrLn "The candidates are:\n"
   print $ candidates
+  -- cleaning section --
+  let firstPass = cleanRoundOne csvData
+  let stars = discardFromListOfLists (== "*") firstPass
+  let rmBlanks = discardFromListOfLists (== "") stars
+  let rmNullLists = discardFromList (== []) rmBlanks
+  let numbers = discardFromList (== candidates) rmNullLists
+  let votes = removeVoteNumberAndName numbers
+  print $ votes
 
 -- stack ghci to test
--- src/Cleaner/CleaningVotes.hs
+++ src/Cleaner/CleaningVotes.hs
@@ -2,9 +2,17 @@ module Cleaner.CleaningVotes where
 
 import Data.List.Split (splitOn)
 
--- not yet implemented
 cleanRoundOne :: String -> [[String]]
 cleanRoundOne dirtyVotes = map (splitOn ",") $ splitOn "\n" dirtyVotes
+
+removeVoteNumberAndName :: [[String]] -> [[String]]
+removeVoteNumberAndName xs = [drop 2 x | x <- xs]
+
+discardFromListOfLists :: (a -> Bool) -> [[a]] -> [[a]]
+discardFromListOfLists p xs = map (\list -> discardFromList p list) xs
+
+discardFromList :: (a -> Bool) -> [a] -> [a]
+discardFromList p xs = [x | x <- xs, not $ p x]
 
 getCandidates :: String -> [String]
 getCandidates firstpass = drop 2 $ head (cleanRoundOne firstpass)
